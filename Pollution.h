6725497e6aa7cc02be2643bc58f0fd4e5852c5ff
gumpop <malloriem123@gmail.com>
2023-03-27 17:42:25 -0500

    Shells Created

---
+++ Pollution.h
@@ -0,0 +1,15 @@
+
+#ifndef PROJECT_1_POLLUTION_H
+#define PROJECT_1_POLLUTION_H
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Pollution{
+    public:
+        PollutionUpdate(vector<vector<Cell*>> map, int &availWorker, int &availGood);
+    private:
+        //add private vars and functions
+};
+#endif //PROJECT_1_POLLUTION_H
