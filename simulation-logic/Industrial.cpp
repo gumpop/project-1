34be2c750a166dbdc6728c1fd72c7d14f4fb5e44
Nelson, Kyndra <kyndranelson@my.unt.edu>
2023-03-29 22:22:57 +0000

    Add iteration through vector to find industrial cells and printing the vector

--- simulation-logic/Industrial.cpp
+++ simulation-logic/Industrial.cpp
@@ -1,6 +1,23 @@
-
 #include "Industrial.h"
-
+//Iterating through the vector and checking to see if a cell is an industrial cell
 Industrial::IndustrialUpdate(vector<vector<Cell*>> map, int &availWorker, int &availGood){
-//here, iterate through vector given and update any cell that is yours based on rules provided
+    for(int i = 0;i!=map.size(); i++){
+        for(int j = 0;j!=map[i].size(); j++){
+            if(map[i][j]->getType() == 1){
+                cout << "INDUSTRIAL FOUND" << endl;
+            }
+        }
+        cout << endl;
+    }
+    //here, iterate through vector given and update any cell that is yours based on rules provided
+}
+
+//Displays the number related to the enum, not the actual variable
+void Industrial::DisplayVector(vector<vector<Cell*>> map){
+  for(int i = 0;i!=map.size(); i++){
+    for(int j = 0;j!=map[i].size(); j++){
+      cout << map[i][j]->getType() << " ";
+    }
+    cout << endl;
+  }
 }
--- simulation-logic/Industrial.h
+++ simulation-logic/Industrial.h
@@ -1,15 +1,18 @@
-
 #ifndef PROJECT_1_INDUSTRIAL_H
 #define PROJECT_1_INDUSTRIAL_H
 
 #include <iostream>
 #include <vector>
-
 using namespace std;
+
+#include "Cell.h"
 
 class Industrial{
     public:
-        IndustrialUpdate(vector<vector<Cell*>> map, int &availWorker, int &availGood);
+        //Change the variable at the front later to call back the right variable "void"
+        void IndustrialUpdate(vector<vector<Cell*>> map, int &availWorker, int &availGood);
+        //Test display vector so that I can make sure my code is running properly
+        void DisplayVector(vector<vector<Cell*>> map);
     private:
         //add private vars and functions
 };
