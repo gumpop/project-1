b2d52ed7babe7b824fc979258abbac80482bc1f7
Cocks, Ryan <ryancocks@my.unt.edu>
2023-04-06 00:20:49 +0000

    Updated to match merged pollution.h

--- simulation-logic/Pollution.h
+++ simulation-logic/Pollution.h
@@ -1,16 +1,58 @@
-
-#ifndef PROJECT_1_POLLUTION_H
-#define PROJECT_1_POLLUTION_H
 #include <iostream>
 #include <vector>
-#include "models/Cell.h"
+#include "Cell.h"
 
 using namespace std;
 
-class Pollution{
-    public:
-        void PollutionUpdate(vector<vector<Cell*>> map, int &availWorker, int &availGood);
+//Defining Pollution class
+class Pollution {
     private:
-        //add private vars and functions
+        //Private Variables
+        vector<vector<int>> pollMap;    //Stores map of pollution values (integers, 0 to max population).
+        int rows, cols;                 //Stores number of rows (up and down) and columns (left to right).
+
+
+    public:
+        //Public Methods
+
+        //Default Constructor
+        Pollution();
+
+        //Parameterized Constructor
+        Pollution(int numRows, int numCols);
+
+        //Sets and Gets
+        int GetRows() {
+            return rows;
+        }
+        int GetCols() {
+            return cols;
+        }
+        vector<vector<int>> GetPollMap() {
+            return pollMap;
+        }
+        void SetRows(int newRows) {
+            rows = newRows;
+        }
+        void SetCols(int newCols) {
+            cols = newCols;
+        }
+        void SetPollMap(vector<vector<int>> newPollMap) {
+            pollMap = newPollMap;
+        }
+
+        //Prints map of pollution integers (0 to max population).
+        void PrintPollutionMap();
+
+        //Prints sum of all integer values in popution map ([row1,col1] + [row1,col2] + ... + [lastrow,lastcol]).
+        void PrintTotalPollution();
+
+        //Prints map of pollution integers within specified region.
+        void PrintSpecRegion(int startRow, int startCol, int endRow, int endCol);
+
+        //Function that looks at each of its neighbors to spread and assign pollution
+        int CheckNeighbors(int currPollution,int currRow, int currCol);
+
+        //Updates the pollution map to correct integers.
+        void Update(vector<vector<Cell*>> cellMap);
 };
-#endif //PROJECT_1_POLLUTION_H
