bd768ce2bf113d3cd53407b7a91934bf505cdb73
Hunt, Ian <ianhunt2@my.unt.edu>
2023-04-28 20:37:48 +0000

    Update Happiness.h

--- Happiness.h
+++ Happiness.h
@@ -1,4 +1,4 @@
-#include "Person.h"
+##include "Person.h"
 #include "Pollution.h"
 #include <iostream>
 #include <string>
@@ -20,9 +20,12 @@ class Happiness {
         //Pass in vector of Worker objects (reference), and the pollution object (reference). 
         //Updates happiness for each worker based on a
         //set of rules defined in RuleCheck() function.
-        void Update(vector<vector<Cell *>> cellMap, vector<Person*> &peopleList, Pollution &pollution);
+        void Update(vector<vector<Cell *>> cellMap, vector<Person*> &peopleList, vector<vector<int>> newPollMap, int pollutionTolerance);
 
         //Applies the set of rules for happiness, called by Update() function.
         //Rules include, but are not limited to: Salary amount, adjacency to pollution at redisence or job, has a job.
-        float RuleCheck(Person* person, Pollution &pollution, int x, int y);
+        float RuleCheck(Person* person, vector<vector<int>> newPollMap, int pollutionTolerance, int x, int y);
+
+        //Checks if there is pollution greater than 1 nearby target coordinates.
+        bool PollutionNearby(vector<vector<int>> newPollMap, int pollutionTolerance, int xCoord, int yCoord);
 };
