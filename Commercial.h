c9e40c3aa498a16ad3c031400f16fbbd0a2d14f9
Kalyan Adhikari <Kalyanadhikari087@gmail.com>
2023-04-28 14:46:03 -0500

    This code is missing some key features but is ready to be merged for testing in meeting. Contains no merging issues due to commerical to my best knowledge.

--- Commercial.h
+++ Commercial.h
@@ -5,15 +5,24 @@
 #include <vector>
 #include "Cell.h"
 #include "Person.h"
+#include "Good.h"
 
 using namespace std;
 
 
 class Commercial {
+private:
+    int availableWorkerNext = 0;
+    int availableWorker = 0;
+
+    int availableGoodNext = 0;
+    int availableGood = 0;
+
+    int goodListCounter = 0;
 public:
-    void CommercialUpdate(vector<vector<Cell*>> map, int &availWorker, int &availGood, int &tempAvailWorker, int &tempAvailGood, vector<Person*> &peopleList);
-    void CommercialCheck(vector<vector<Cell*>> map, int i, int j, int boundsi, int boundsj, int &availWorker, int &availGood, int &tempAvailWorker, int &tempAvailGood);
-    void UpdateTimestamp(vector<vector<Cell*>> map,int &availWorker, int&availGood,  vector<Person*> &peopleList);
+    void CommercialUpdate(vector<vector<Cell*>> map, vector<Person*> &peopleList, vector<Good*> &goodList, int &peopleListCounter);
+    void CommercialCheck(vector<vector<Cell*>> map, int i, int j, int boundsi, int boundsj, int availableWorkerNext, int availableGoodNext);
+    void UpdateTimestamp(vector<vector<Cell*>> map,int availableWorker, int availableGood,  vector<Person*> &peopleList, int &peopleListCounter, vector<Good*> &goodList);
 
 
 private:
